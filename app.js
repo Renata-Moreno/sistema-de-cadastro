d1fe2ad176e7bc627790404b00ec05d2d7609fad
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2023-01-30 18:59:40 -0300

    implementando mock de notificação de app no ar

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectando ao banco de dados")
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("aplicação está se conectando ao banco de dados");
+console.log("aplicação está se notificando que esta no ar");
